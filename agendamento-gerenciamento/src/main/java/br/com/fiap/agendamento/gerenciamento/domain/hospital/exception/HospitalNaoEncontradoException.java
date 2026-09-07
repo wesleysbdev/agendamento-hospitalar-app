873a373371013ad1e829ee93a2ce04088dbcba46
Junior G <juniorgeo70@hotmail.com>
2026-09-06 21:42:05 -0300

    fix: corrigindo expection

--- agendamento-gerenciamento/src/main/java/br/com/fiap/agendamento/gerenciamento/domain/hospital/exception/HospitalNaoEncontradoException.java
+++ agendamento-gerenciamento/src/main/java/br/com/fiap/agendamento/gerenciamento/domain/hospital/exception/HospitalNaoEncontradoException.java
@@ -1,8 +1,8 @@
 package br.com.fiap.agendamento.gerenciamento.domain.hospital.exception;
 
-import br.com.fiap.agendamento.gerenciamento.domain.exception.RecursoNaoEncontradoException;
+import br.com.fiap.agendamento.gerenciamento.domain.exception.RegraDeNegocioException;
 
-public class HospitalNaoEncontradoException extends RecursoNaoEncontradoException {
+public class HospitalNaoEncontradoException extends RegraDeNegocioException {
     public HospitalNaoEncontradoException(String message) {
         super(message);
     }
