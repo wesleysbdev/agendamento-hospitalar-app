6e0d441ae1305aa0ab559d23a0274a9513ee3b23
Junior G <juniorgeo70@hotmail.com>
2026-09-16 18:22:55 -0300

    feat: flyway to create user admin

---
+++ agendamento-gerenciamento/src/main/resources/db/migration/gerenciamento/V30000__criacao_usuario_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.usuario(
+    tipo, id, ativo, criado_em, email, excluido, nome, senha, crm, telefone)
+VALUES ('ADMINISTRADOR', 'f164f8d0-c9f9-4ad3-85a5-d7af4fcd08e6', true, now(), 'admin@email.com', false, 'Usuaŕio Admin', '$2a$10$BLECkLhNWI7dXkvF5egCEOB7vLQgRDxwaPYV3e/pq7JIcg5O1CSPK', null, null);
